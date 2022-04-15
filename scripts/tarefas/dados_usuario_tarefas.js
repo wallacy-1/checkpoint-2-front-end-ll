b1d964ab0b82c34cdbc18efd15c4a9b81185110a
Jonata <Jonata994@gmail.com>
2022-04-15 14:20:32 -0300

    atualização

--- scripts/tarefas/dados_usuario_tarefas.js
+++ scripts/tarefas/dados_usuario_tarefas.js
@@ -1,16 +1,60 @@
 onload = function(){
-    let skeleton = document.getElementById('skeleton');
 
-    let tarefa = document.createElement('li');
-    tarefa.classList.add('tarefa');
 
-    tarefa.innerHTML = `
-        <div class="not-done"> </div>
-        <div class="descricao">
-            <p class="nome">Teste </p>
-            <p class="timestamp">Criada em: 18/01/18</p>
-        </div>
-    `
+    let urlexibirtarefa = 'https://ctd-todo-api.herokuapp.com/v1/tasks';
 
-    skeleton.appendChild(tarefa);
+    let tokenjwt = localStorage.getItem('token');
+
+    let header = {
+        method: "GET",
+        headers: {
+            'content-type': 'application/json',
+            'authorization': tokenjwt
+        }
+
+    };
+
+    fetch(urlexibirtarefa, header).then(
+        resultado => {
+            console.log(resultado);
+            return resultado.json()
+
+        }
+    )
+        .then(
+            resultado => {
+                console.log(resultado)
+                exibirtarefa(resultado)
+            }
+        )
+
+        .catch(
+            erro => {
+                alert(erro)
+            }
+        )
+
+
+    function exibirtarefa(listatarefa) {
+        let skeleton = document.getElementById('skeleton');
+
+
+
+        listatarefa.forEach(tarefacriada => {
+            let tarefa = document.createElement('li');
+            tarefa.classList.add('tarefa');
+            tarefa.innerHTML = `
+                <div class="not-done"> </div>
+                <div class="descricao">
+                    <p class="nome">${tarefacriada.description}</p>
+                    <p class="timestamp">Criada em: ${tarefacriada.createdAt}</p>
+                </div >
+            `
+
+            skeleton.appendChild(tarefa);
+        });
+
+    }
+
 };
+
